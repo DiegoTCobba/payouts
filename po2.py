272d3c78f2336162c356afa236576b351888fb2f
DiegoTCobba <diegot.cobba@gmail.com>
2025-05-01 11:21:19 -0500

    Update po2.py

--- po2.py
+++ po2.py
@@ -189,3 +189,6 @@ with preRech:
                 file_name="filtrado_por_filas_pdf.xlsx",
                 mime="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
             )
+
+        except Exception as e:
+            st.error(f"Ocurrió un error: {e}")
