2db34a58b66ef91e1074a449a062aa4d27e3133f
DiegoTCobba <diegot.cobba@gmail.com>
2025-04-30 23:58:02 -0500

    Update po.py

--- po.py
+++ po.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
-import PyMuPDF    
+import PyPDF2 openai PyMuPDF #    
 import io
 from openpyxl import load_workbook
 from openpyxl.styles import PatternFill
