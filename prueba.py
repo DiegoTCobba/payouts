117bfcd6fcdd0de4596edb8e498ab41b75210229
DiegoTCobba <diegot.cobba@gmail.com>
2025-05-02 13:18:05 -0500

    Update prueba.py

--- prueba.py
+++ prueba.py
@@ -1,1 +1,220 @@
+import streamlit as st
+import pandas as pd
+import fitz  # PyMuPDF
+import io
+from openpyxl import load_workbook, Workbook
+from openpyxl.styles import PatternFill
+from openpyxl.utils import get_column_letter
+import re
 
+st.title("RECHAZOS DE PAGOS MASIVOS ")
+st.divider()
+
+# Crear pestañas
+preRech, postRech = st.tabs(["📄 PRE RECHAZOS BCP", "📄 POST RECHAZOS"])
+
+
+with postRech:
+    st.subheader("POST RECHAZOS MASIVOS")
+    st.divider()
+
+    st.write("Herramienta diseñada para extraer DNI desde un archivo PDF y filtrar automáticamente en la base de datos de un Excel")
+    
+    # Tabla con códigos de rechazo
+    st.markdown("### Códigos de Rechazo Frecuentes")
+    codigos_rechazo = {
+        "Código": ["R001", "R002", "R007"],
+        "Descripción": ["DOCUMENTO ERRADO", "CUENTA INVALIDA", "RECHAZO POR CCI"]}
+    
+    df_codigos = pd.DataFrame(codigos_rechazo)
+    st.table(df_codigos)
+    
+    # Subir de archivos pdf y excel
+    st.markdown("### ARCHIVO PDF")
+    pdf_file = st.file_uploader("Sube un archivo PDF", type="pdf")
+    st.markdown("### MASIVO EXCEL")
+    excel_file = st.file_uploader("Sube un archivo Excel", type=["xlsx"])
+
+    if pdf_file and excel_file:
+        # Extraer texto del PDF
+        text = ""
+        with fitz.open(stream=pdf_file.read(), filetype="pdf") as doc:
+            for page in doc:
+                text += page.get_text()
+
+        # Eliminar posibles números de cuenta como 380-75148297-0-31
+        cuentas_posibles = re.findall(r'\b[\d]{2,4}-[\d]{5,10}-\d{1,2}-\d{1,3}\b', text)
+        for cuenta in cuentas_posibles:
+            text = text.replace(cuenta, '')
+        # Buscar pares (DNI, MONTO)
+        dni_monto_pairs = re.findall(r'(\d{6,})\s+[A-ZÁÉÍÓÚÑ ]+\s+[0-9\-]+[\s\S]*?S/\s*([\d.,]+)', text)
+
+        # Limpiar montos y eliminar duplicados
+        dni_monto_cleaned = set()
+        for dni, monto in dni_monto_pairs:
+            # Estandarizar formato decimal
+            monto = monto.replace(",", "")
+            dni_monto_cleaned.add((dni, monto))
+  
+        # Reposicionar puntero para openpyxl
+        excel_file.seek(0)
+        wb = load_workbook(excel_file)
+        ws = wb.active
+
+        # Resaltado relleno amarillo
+        fill = PatternFill(start_color="FFFF00", end_color="FFFF00", fill_type="solid")
+
+        # Columnas a ocultar en el excel
+        columnas_a_ocultar = ['B', 'C', 'E', 'F', 'G', 'H', 'J', 'K', 'L', 'N', 'O', 'P','Q']
+        for col in columnas_a_ocultar:
+            ws.column_dimensions[col].hidden = True
+
+        # Filtrar filas con coincidencias
+        filas_con_coincidencias = []
+        for row in ws.iter_rows(min_row=2, values_only=False):
+            celdas = [str(cell.value).replace(",", "") if cell.value is not None else "" for cell in row]
+            for dni, monto in dni_monto_cleaned:
+                if dni in celdas and monto in celdas:
+                    filas_con_coincidencias.append([cell.value for cell in row])
+                    break
+
+        # Crear excel nuevo
+        wb_filtrado = Workbook()
+        ws_filtrado = wb_filtrado.active
+
+        # Encabezados
+        encabezados = [cell.value for cell in ws[1]]
+        ws_filtrado.append(encabezados)
+
+        # Agregar filas filtradas
+        for row in filas_con_coincidencias:
+            ws_filtrado.append(row)
+
+        # Aplicar resaltado
+        for row in ws_filtrado.iter_rows(min_row=2):
+            for cell in row:
+                if str(cell.value) in documentos_set:
+                    cell.fill = fill
+
+        # Ocultar columnas
+        for col in columnas_a_ocultar:
+            ws_filtrado.column_dimensions[col].hidden = True
+
+        #Mantener ancho de columnas no ocultas
+        from openpyxl.utils import get_column_letter
+        for col_idx in range(1, ws.max_column + 1):
+            letra = get_column_letter(col_idx)
+            if letra not in columnas_a_ocultar:
+                if letra in ws.column_dimensions and ws.column_dimensions[letra].width:
+                    ancho = ws.column_dimensions[letra].width
+                    ws_filtrado.column_dimensions[letra].width = ancho
+
+        # Guardar nuevo archivo en memoria
+        output_filtrado = io.BytesIO()
+        wb_filtrado.save(output_filtrado)
+        output_filtrado.seek(0)
+
+        # Mostrar vista previa
+        df_filtrado = pd.DataFrame(filas_con_coincidencias, columns=encabezados)
+        letras_a_indices = [ord(c) - ord('A') for c in columnas_a_ocultar]
+        columnas_visibles = [col for idx, col in enumerate(df_filtrado.columns) if idx not in letras_a_indices]
+        df_visible = df_filtrado[columnas_visibles]
+
+        st.subheader("Vista previa final con filas y columnas filtradas:")
+        st.dataframe(df_visible)
+
+        # Botón descarga
+        st.download_button(
+            label="Descargar Excel con resaltado y filas filtradas",
+            data=output_filtrado,
+            file_name="resaltado_filtrado.xlsx",
+            mime="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet")
+#--------------------------------------------------------------------------------------
+with preRech:
+    st.subheader("PRE RECHAZOS MASIVOS")
+    st.divider()
+    st.write("Herramienta diseñada para extraer N° de fila desde un archivo PDF y filtrar esas filas del Excel")
+
+        # Tabla con códigos de rechazo
+    st.markdown("### Códigos de Rechazo Frecuentes")
+    codigos_rechazo = {
+        "Código": ["R001", "R002", "R007"],
+        "Descripción": ["DOCUMENTO ERRADO", "CUENTA INVALIDA", "RECHAZO POR CCI"]}
+    
+    df_codigos = pd.DataFrame(codigos_rechazo)
+    st.table(df_codigos)
+    st.markdown("### ARCHIVO PDF")
+    pdf_file_filas = st.file_uploader("Sube un archivo PDF con números de fila", type="pdf", key="pdf_filas")
+    st.markdown("### MASIVO EXCEL")
+    excel_file_filas = st.file_uploader("Sube un archivo Excel", type=["xlsx"], key="excel_filas")
+
+    if pdf_file_filas and excel_file_filas:
+        try:
+            # Leer PDF y extraer texto
+            text = ""
+            with fitz.open(stream=pdf_file_filas.read(), filetype="pdf") as doc:
+                for page in doc:
+                    text += page.get_text()
+
+            # Extraer números de fila (1 o más dígitos)
+            numeros_filas = re.findall(r'Registro\s+(\d+)', text, re.IGNORECASE)
+            numeros_filas = list(set(int(n) + 1 for n in numeros_filas if int(n) >= 1))
+
+
+            # Cargar Excel
+            wb = load_workbook(excel_file_filas)
+            ws = wb.active
+            encabezados = [cell.value for cell in ws[1]]
+            filas_seleccionadas = []
+
+            for i in numeros_filas:
+                if i <= ws.max_row:
+                    fila = [cell.value for cell in ws[i]]
+                    filas_seleccionadas.append(fila)
+
+            # Crear nuevo archivo
+            wb_nuevo = Workbook()
+            ws_nuevo = wb_nuevo.active
+            ws_nuevo.append(encabezados)
+
+            fill = PatternFill(start_color="FFFF00", end_color="FFFF00", fill_type="solid")
+            columnas_a_ocultar = ['B', 'C', 'E', 'F', 'G', 'H', 'J', 'K', 'L', 'N', 'O', 'P','R']
+
+            for fila in filas_seleccionadas:
+                ws_nuevo.append(fila)
+
+            for row in ws_nuevo.iter_rows(min_row=2):
+                for cell in row:
+                    cell.fill = fill
+
+            for col in columnas_a_ocultar:
+                ws_nuevo.column_dimensions[col].hidden = True
+
+            for col_idx in range(1, ws.max_column + 1):
+                letra = get_column_letter(col_idx)
+                if letra not in columnas_a_ocultar:
+                    if letra in ws.column_dimensions and ws.column_dimensions[letra].width:
+                        ws_nuevo.column_dimensions[letra].width = ws.column_dimensions[letra].width
+
+            output = io.BytesIO()
+            wb_nuevo.save(output)
+            output.seek(0)
+
+            # Mostrar vista previa
+            df_filtrado = pd.DataFrame(filas_seleccionadas, columns=encabezados)
+            letras_a_indices = [ord(c) - ord('A') for c in columnas_a_ocultar]
+            columnas_visibles = [col for idx, col in enumerate(df_filtrado.columns) if idx not in letras_a_indices]
+            df_visible = df_filtrado[columnas_visibles]
+
+            st.subheader("Vista previa de filas seleccionadas:")
+            st.dataframe(df_visible)
+
+            st.download_button(
+                label="Descargar Excel filtrado",
+                data=output,
+                file_name="filtrado_por_filas_pdf.xlsx",
+                mime="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+            )
+
+        except Exception as e:
+            st.error(f"Ocurrió un error: {e}")
